51db63d70499dda1f9b44cce2ce9ee6a63defda8
PremnathSundarrajan <premsundarrajan4@gmail.com>
2026-02-05 05:49:40 +0530

    backhaul related api's added

--- routes/backhaul.js
+++ routes/backhaul.js
@@ -5,6 +5,25 @@ const { authenticateToken } = require('../middleware/auth');
 
 router.use(authenticateToken);
 
+// Get backhaul opportunities
 router.get('/opportunities', backhaulController.getOpportunities);
+
+// Check for new backhaul opportunities (called after delivery completion)
+router.post('/check-opportunities', backhaulController.checkOpportunities);
+
+// Accept backhaul opportunity
+router.post('/:id/accept', backhaulController.acceptBackhaul);
+
+// Reject backhaul opportunity
+router.post('/:id/reject', backhaulController.rejectBackhaul);
+
+// Start pickup (en route to shipper)
+router.post('/:id/start-pickup', backhaulController.startPickup);
+
+// Confirm pickup (cargo loaded)
+router.post('/:id/confirm-pickup', backhaulController.confirmPickup);
+
+// Complete backhaul delivery
+router.post('/:id/complete', backhaulController.completeBackhaul);
 
 module.exports = router;
